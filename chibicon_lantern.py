8bb8a0cd2dd2e727a982ad3c8cd43180b43c2233
Marcel Wegmann <wegr@zhaw.ch>
2024-05-30 15:36:25 +0200

    updated queststand-light

--- chibicon_lantern.py
+++ chibicon_lantern.py
@@ -1,8 +1,10 @@
 from machine import Pin
 from neopixel import NeoPixel
+from utime import sleep_ms
 
-def getRGB(deg):
-    m=1/60
+
+def rainbow(deg):
+    m=1./60.
     if deg>=0 and deg<60:
         R=1
         G=0
@@ -27,62 +29,106 @@ def getRGB(deg):
         R=1
         G=1-m*(deg-300)
         B=0
-    myColor=(R,G,B)
+    myColor=[int(R*255) ,int(G*255) ,int(B*255)]
     return myColor
         
-def bottle(n,np):
+def bottle(n,np, color):
     if n > 4:
         return
-    if bottle.iterator[n]+bottle.offset[n] < 360:
-    	bottle.iterator[n] += bottle.offset[n]
-    else:
-    	bottle.iterator[n] = bottle.iterator[n]+bottle.offset[n]-360
-    np [2*n] = getRGB(bottle.iterator)
+    np [2*n] = color
     np [2*n+1] = np[2*n] 
-	
-def lantern(np):
-    if lantern.iterator+lantern.offset < 360:
-        lantern.iterator += lantern.offset
-    else:
-        lantern.iterator = lantern.iterator+lantern.offset-360
-    np[14] = getRGB(lantern.iterator)
+    
+def lantern(np, color):
+    np[14] = color
     np[15] = np[14]
     np[16] = np[14]
     np[17] = np[14]
 
 
-def crystal(np):
-    if crystal.iterator+crystal.offset < 360:
-        crystal.iterator += crystal.offset
-    else:
-        crystal.iterator = crystal.iterator+crystal.offset-360
-    np[10] = getRGB(crystal.iterator)
+def crystal(np, color):
+    np[10] = color
     np[11] = np[10]
     np[12] = np[10]
     np[13] = np[10]
 
 
-bottle.iterator={0,0,0,0,0}
-lantern.iterator=0
-crystal.iterator=0
-bottle.offset={2,3,3,2,2}
-lantern.offset=2
-crystal.offset=3
 
+def chibicolors(n): # n is of 1 to 360
+    if n > 180:
+        n = 180-(n-180)
+    r = 193+(25./180.*n)
+    g = 131-(92./180.*n)
+    b = 251-(131./180.*n)
+    return [int(r) ,int(g) ,int(b)]
+
+def white_brightness(n):
+    if n > 180:
+        n = 180-(n-180)
+    w = 255-(n)
+    r = w
+    g = w
+    b = w
+    return [int(r) ,int(g) ,int(b)]   
+
+def iterate_counters(n, step_size):
+    if (n+step_size >= 360):
+        n=0
+    else:
+        n=n+step_size
+    return n
+
+bottle_color_0 = (100,0,0)
+bottle_color_1 = (0,100,0)
+bottle_color_2 = (0,0,100)
+bottle_color_3 = (100,100,0)
+bottle_color_4 = (0,100,100)
+crystal_color = (100,0,100)
+lantern_color = (100,100,100)
+
+bottle_counter_0 = 0
+bottle_counter_1 = 0
+bottle_counter_2 = 0
+bottle_counter_3 = 0
+bottle_counter_4 = 0
+crystal_counter  = 50
+lantern_counter  = 0
+
+
+bottle_step_size_0 = 1
+bottle_step_size_1 = 1
+bottle_step_size_2 = 1
+bottle_step_size_3 = 1
+bottle_step_size_4 = 1
+crystal_step_size  = 3
+lantern_step_size  = 2
 
 gpio = 21
 led_pin = Pin(gpio,Pin.OUT)
-led_count = 4
+led_count = 18
 np = NeoPixel(led_pin,led_count)
 
 
-for i in range(4):
-    bottle(0,np)
-    bottle(1,np)
-    bottle(2,np)
-    bottle(3,np)
-    bottle(4,np)
-    crystal(np)
-    lantern(np)
-np.write()
+while True:
+    sleep_ms(100)
+    bottle_counter_0 = iterate_counters(bottle_counter_0, bottle_step_size_0)
+    bottle_counter_1 = iterate_counters(bottle_counter_1, bottle_step_size_1)
+    bottle_counter_2 = iterate_counters(bottle_counter_2, bottle_step_size_2)
+    bottle_counter_3 = iterate_counters(bottle_counter_3, bottle_step_size_3)
+    bottle_counter_4 = iterate_counters(bottle_counter_4, bottle_step_size_4)
+    crystal_counter = iterate_counters(crystal_counter, crystal_step_size)
+    lantern_counter = iterate_counters(lantern_counter, lantern_step_size)
+
+    bottle_color_0 = white_brightness(bottle_counter_0)
+    print(crystal_color)
+    crystal_color = chibicolors(crystal_counter)
+    crystal_color = rainbow(lantern_counter)
+    lantern_color = chibicolors(lantern_counter)
+    bottle(0,np, bottle_color_0)
+    bottle(1,np, bottle_color_1)
+    bottle(2,np, bottle_color_2)
+    bottle(3,np, bottle_color_3)
+    bottle(4,np,  bottle_color_4)
+    crystal(np, crystal_color)
+    lantern(np, lantern_color)
+    np.write()
         
